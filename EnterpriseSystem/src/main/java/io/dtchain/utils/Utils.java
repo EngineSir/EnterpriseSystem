061a806013939aaaac3c695ff04975027bab5178
EngineSir <arc_eng@163.com>
2019-01-03 15:37:37 +0800

    增加shiro密码加密算法md5

--- EnterpriseSystem/src/main/java/io/dtchain/utils/Utils.java
+++ EnterpriseSystem/src/main/java/io/dtchain/utils/Utils.java
@@ -2,14 +2,20 @@ package io.dtchain.utils;
 
 import java.util.UUID;
 
-public class Utils
-{
+import org.apache.shiro.crypto.hash.SimpleHash;
+import org.apache.shiro.util.ByteSource;
+
+public class Utils {
 	/*
 	 * 利用UUID算法生成主键
 	 */
-	public static String createId(){
-		UUID uuid=UUID.randomUUID();
-		String id=uuid.toString();
-		return id.replace("-","");
+	public static String createId() {
+		UUID uuid = UUID.randomUUID();
+		String id = uuid.toString();
+		return id.replace("-", "");
+	}
+	
+	public static String Md5(String userName,String num) {
+		return new SimpleHash("MD5", num,  ByteSource.Util.bytes(userName), 2).toHex();
 	}
 }
