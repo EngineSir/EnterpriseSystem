307d94ea52dba78726277f9941f17b6f780dcbd0
EngineSir <arc_eng@163.com>
2019-01-03 15:13:49 +0800

    上传文件service接口

--- EnterpriseSystem/src/main/java/io/dtchain/service/UpLoadService.java
+++ EnterpriseSystem/src/main/java/io/dtchain/service/UpLoadService.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import io.dtchain.entity.QueryRecord;
 import io.dtchain.entity.ResultProce;
 import io.dtchain.utils.Result;
