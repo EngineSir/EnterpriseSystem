1588c982d9bbec646f5b521c6c553625d3434c79
EngineSir <arc_eng@163.com>
2019-01-04 16:22:09 +0800

    公告页面详情接口

---
+++ EnterpriseSystem/src/main/java/io/dtchain/controller/NoticePageController.java
@@ -0,0 +1,22 @@
+package io.dtchain.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
+@Controller
+@Api(description = "公告页面显示接口")
+@CrossOrigin
+public class NoticePageController {
+	
+	@ApiOperation(value = "显示公告详情页面")
+	@GetMapping(value="/notice.io")
+	public String blogPage(@ApiParam(value = "公告id", required = true) @RequestParam(value="id") String id){
+		return "noticePage";
+	}
+}
