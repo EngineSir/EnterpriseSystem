ade27aa24eb2be7dfe423d7b56766e7ee16790e2
EngineSir <arc_eng@163.com>
2019-01-07 16:39:26 +0800

    修改请假属性

--- EnterpriseSystem/src/main/java/io/dtchain/entity/LeaveTable.java
+++ EnterpriseSystem/src/main/java/io/dtchain/entity/LeaveTable.java
@@ -1,48 +1,85 @@
 package io.dtchain.entity;
 
+import java.sql.Timestamp;
+
 public class LeaveTable
 {
-	private String empName;
-	private String dept;
-	private String startDate;
-	private String endDate;
-	public String getEmpName()
-	{
-		return empName;
+	private String id;
+	private String leaveType;
+	private String startTime;
+	private String endTime;
+	private int leaveNum;
+	private Timestamp createTime;
+	private String leaveRegard;
+	private String approver;
+	private int  approverStatue;
+	private String applicant;
+	public String getId() {
+		return id;
 	}
-	public void setEmpName(String empName)
-	{
-		this.empName = empName;
+	public void setId(String id) {
+		this.id = id;
 	}
-	public String getDept()
-	{
-		return dept;
+	public String getLeaveType() {
+		return leaveType;
 	}
-	public void setDept(String dept)
-	{
-		this.dept = dept;
+	public void setLeaveType(String leaveType) {
+		this.leaveType = leaveType;
 	}
-	public String getStartDate()
-	{
-		return startDate;
+	public String getStartTime() {
+		return startTime;
 	}
-	public void setStartDate(String startDate)
-	{
-		this.startDate = startDate;
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
 	}
-	public String getEndDate()
-	{
-		return endDate;
+	public String getEndTime() {
+		return endTime;
 	}
-	public void setEndDate(String endDate)
-	{
-		this.endDate = endDate;
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+	public int getLeaveNum() {
+		return leaveNum;
+	}
+	public void setLeaveNum(int leaveNum) {
+		this.leaveNum = leaveNum;
+	}
+	public Timestamp getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Timestamp createTime) {
+		this.createTime = createTime;
+	}
+	public String getLeaveRegard() {
+		return leaveRegard;
+	}
+	public void setLeaveRegard(String leaveRegard) {
+		this.leaveRegard = leaveRegard;
+	}
+	public String getApprover() {
+		return approver;
+	}
+	public void setApprover(String approver) {
+		this.approver = approver;
+	}
+	public int getApproverStatue() {
+		return approverStatue;
+	}
+	public void setApproverStatue(int approverStatue) {
+		this.approverStatue = approverStatue;
+	}
+	public String getApplicant() {
+		return applicant;
+	}
+	public void setApplicant(String applicant) {
+		this.applicant = applicant;
 	}
 	@Override
-	public String toString()
-	{
-		return "LeaveTable [empName=" + empName + ", dept=" + dept + ", startData=" + startDate + ", endData=" + endDate
-		        + "]";
+	public String toString() {
+		return "LeaveTable [id=" + id + ", leaveType=" + leaveType + ", startTime=" + startTime + ", endTime=" + endTime
+				+ ", leaveNum=" + leaveNum + ", createTime=" + createTime + ", leaveRegard=" + leaveRegard
+				+ ", approver=" + approver + ", approverStatue=" + approverStatue + ", applicant=" + applicant + "]";
 	}
+	
 	
 }
