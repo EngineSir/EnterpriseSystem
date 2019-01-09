9a5457538f9ba9606ec159e692da4820c0c2b0ba
EngineSir <arc_eng@163.com>
2019-01-09 18:34:32 +0800

    解决servletfileupload与springboot默认上传组件冲突问题

--- EnterpriseSystem/src/main/java/io/dtchain/controller/UpLoadController.java
+++ EnterpriseSystem/src/main/java/io/dtchain/controller/UpLoadController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import io.dtchain.entity.QueryRecord;
 import io.dtchain.entity.ResultProce;
@@ -34,8 +35,8 @@ public class UpLoadController
 	
 	@ApiOperation(value = "上传excel文件")
 	@PostMapping(value="/upload.io")
-	public void upLoad(HttpServletRequest req,HttpServletResponse res)throws Exception{
-		upLoadService.upLoad(req, res);
+	public void upLoad(@ApiParam(value = "上传excel", required = true) @RequestParam(value = "file") MultipartFile file,HttpServletRequest req,HttpServletResponse res)throws Exception{
+		upLoadService.upLoad(file,req, res);
 	}
 	
 	@ApiOperation(value = "查询双休工时统计")
