152a4df07d0b45348b767d61a01b3bacb417e961
EngineSir <arc_eng@163.com>
2019-01-10 18:13:05 +0800

    excel格式代码

--- EnterpriseSystem/src/main/java/io/dtchain/utils/Utils.java
+++ EnterpriseSystem/src/main/java/io/dtchain/utils/Utils.java
@@ -2,6 +2,11 @@ package io.dtchain.utils;
 
 import java.util.UUID;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.shiro.crypto.hash.SimpleHash;
 import org.apache.shiro.util.ByteSource;
 
@@ -18,4 +23,24 @@ public class Utils {
 	public static String Md5(String userName,String num) {
 		return new SimpleHash("MD5", num,  ByteSource.Util.bytes(userName), 2).toHex();
 	}
+	public static void excelFormat(HSSFWorkbook wb, HSSFSheet sheet) {
+		HSSFCellStyle style = wb.createCellStyle();
+		style.setAlignment(HSSFCellStyle.ALIGN_CENTER);
+
+		style.setBorderBottom(HSSFCellStyle.BORDER_THIN); // 下边框
+		style.setBorderLeft(HSSFCellStyle.BORDER_THIN);// 左边框
+		style.setBorderTop(HSSFCellStyle.BORDER_THIN);// 上边框
+		style.setBorderRight(HSSFCellStyle.BORDER_THIN);// 右边框
+		for (int j = 1; j <= sheet.getLastRowNum(); j++) {
+			HSSFRow r = sheet.getRow(j);
+			if (r != null) {
+				for (int l = 0; l < r.getLastCellNum(); l++) {
+					HSSFCell c = r.getCell(l);
+					c.setCellStyle(style);
+					sheet.setColumnWidth(l, 20 * 300);
+				}
+			}
+		}
+	}
+	
 }
