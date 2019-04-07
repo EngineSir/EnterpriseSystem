8010cf884fa01d1f3708c248b7dd0f7afe577b42
EngineSir <arc_eng@163.com>
2019-04-07 19:12:12 +0800

    文档中心相关页面及接口

---
+++ EnterpriseSystem/src/main/resources/static/scripts/dataUpload.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	$(".com").load("com");
+	layui.use(['layer','form','upload'],function(){
+		var upload=layui.upload;
+		upload.render({
+		    elem: '#uploadFile'
+		    ,url: '/dataShare/uploadDataFile.io'
+		    ,accept: 'file' //普通文件
+		    ,auto:false
+		    ,data:{docTitle:function(){
+		    	return $(".docTitle").val().trim();
+		    },docContent:function(){
+		    	return $(".remark").val().trim();
+		    }}
+		    ,bindAction:'#upload'
+		    ,done: function(res){
+		      console.log(res);
+		     
+		    }
+		  });
+	});
+});
+
